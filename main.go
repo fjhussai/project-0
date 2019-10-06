7d4f7874631ae269facf77ef9cac976a373bebe1
fjhussai <farwajhussain@gmail.com>
2019-10-06 14:49:33 -0500

    added greeting

--- main.go
+++ main.go
@@ -3,5 +3,31 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, go!")
+	var grting [3]string
+	grting[0] = "Hi there! Welcome to Piggy Bank! \n"
+	grting[1] = "Piggy Bank is here to help you keep track of your savings."
+	grting[2] = "Let's get started!"
+
+	fmt.Println(grting[0], grting[1])
 }
+
+//Have users set a username
+/*
+func getuser() {
+	fmt.Println("Please enter a username below:")
+	var username string
+	fmt.Scanln(&username)
+}
+
+type users struct {
+	username string
+	password string
+	email    string
+}
+
+
+//have users set a password
+//Have the pairs stored in a map
+//Keep track of user data in a structure
+
+*/
